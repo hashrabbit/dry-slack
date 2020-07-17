fd86d60cd2cba02155e7ce30b54b72c423a8ffc3
Brian V. Hughes <brianvh89@gmail.com>
2020-07-17 14:29:45 -0700

    Update PublishWebhook to use DoService. Refactor to remove #render method.

--- lib/dry/slack/publish_webhook.rb
+++ lib/dry/slack/publish_webhook.rb
@@ -1,31 +1,17 @@
 # frozen_string_literal: true
 
-require 'dry/matcher'
-require 'dry/matcher/result_matcher'
-
 module Dry
   module Slack
     class PublishWebhook
       extend Dry::Initializer
-      include Dry::Monads[:result]
-      include Dry::Monads::Do.for(:call)
-      include Dry::Matcher.for(:call, with: Dry::Matcher::ResultMatcher)
+      include DoService
 
       option :url, default: proc { ENV.fetch('SLACK_WEBHOOK', '') }
       option :post_json, default: proc { PostJson.new(url) }
 
       def call(payload)
-        json = yield render(payload)
-        post_json.call(json)
-      end
-
-      private
-
-      def render(payload)
-        payload
-          .validate
-          .to_monad
-          .fmap { payload.to_h.to_json }
+        yield payload.validate.to_monad
+        post_json.call(payload.to_h.to_json)
       end
     end
   end
