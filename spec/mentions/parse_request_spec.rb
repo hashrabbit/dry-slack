7e5d45e802f3c479dda1159d9f350dea9bca44a3
Brian V. Hughes <brianvh89@gmail.com>
2020-07-17 14:38:21 -0700

    Add Mentions::ParseRequest and integration specs
    Ensures that a Slack request meets the basic qualification to be parsed
    into a RequestStruct, for further validation and processing.

---
+++ spec/mentions/parse_request_spec.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+module Dry
+  module Slack
+    module Mentions
+      RSpec.describe ParseRequest do
+        let(:args) { [Time.now.to_i, 'sig', { text: 'foo' }.to_json] }
+        let(:request) { Spec::RequestStub.new(*args) }
+        let(:result) { described_class.new.call(request) }
+
+        context 'with a vaild POST Request object' do
+          it 'returns Success, wrapping a RequestStruct' do
+            expect(result).to be_success
+            expect(result.value!).to be_a(RequestStruct)
+          end
+        end
+
+        context 'with a non-POST Request object' do
+          let(:request) {
+            Spec::RequestStub.new(*args.push('application/json', false))
+          }
+
+          it 'returns Failure, wrapping a "Method not allowed" Error' do
+            expect(result).to be_failure
+            expect(result.failure.text).to eq 'Method not allowed'
+          end
+        end
+
+        context 'with a invalid content-type Request object' do
+          let(:request) { Spec::RequestStub.new(*args.push('foo')) }
+
+          it 'returns Failure, wrapping a "Bad Request" Error' do
+            expect(result).to be_failure
+            expect(result.failure.text).to eq 'Bad Request'
+          end
+        end
+
+        context 'with a Request object that fails #to_struct' do
+          let(:args) { [Time.now.to_i, :sig, { text: 'foo' }.to_json] }
+
+          it 'returns Failure, wrapping a "Invalid Request" Error' do
+            expect(result).to be_failure
+            expect(result.failure.text).to eq 'Invalid Request'
+          end
+        end
+      end
+    end
+  end
+end
